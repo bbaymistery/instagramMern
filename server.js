886dd288083b92bda7562da1530da0605c299598
Elgun <elgun.ezmemmedov@mail.ru>
2023-02-17 18:58:07 +0400

    'getComments'

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ const corsOptions = {
     credentials: true,            //access-control-allow-credentials:true
     optionSuccessStatus: 200
 }
-   
+
+
 app.use(express.json())
 app.use(cors())
 app.use(cookieParser())
