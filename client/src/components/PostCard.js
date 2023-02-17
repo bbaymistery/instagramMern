84e3d6ff9919fd2de21b56155c587e478963c565
Elgun <elgun.ezmemmedov@mail.ru>
2023-02-17 17:49:38 +0400

    's19client'

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -2,6 +2,8 @@ import React from 'react'
 import CardHeader from './home/post_card/CardHeader'
 import CardBody from './home/post_card/CardBody'
 import CardFooter from './home/post_card/CardFooter'
+import Comments from './home/Comments'
+import InputComment from './home/InputComment'
 
 
 const PostCard = ({post, theme}) => {
@@ -10,9 +12,10 @@ const PostCard = ({post, theme}) => {
             <CardHeader post={post} />
             <CardBody post={post} theme={theme} />
             <CardFooter post={post} />
+            <Comments post={post} />
+            <InputComment post={post} />
             {/*
 
-            <Comments post={post} />
             <InputComment post={post} /> */}
         </div>
     )
