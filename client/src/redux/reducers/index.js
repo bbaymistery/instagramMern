d1fbc28e93374f20f8aec1bf81a503113c51a260
Elgun <elgun.ezmemmedov@mail.ru>
2023-02-18 15:44:36 +0400

    s33

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -11,7 +11,7 @@ import discover from './discoverReducer'
 import suggestions from './suggestionsReducer'
 import socket from './socketReducer'
 import notify from './notifyReducer'
-// import message from './messageReducer'
+import message from './messageReducer'
 // import online from './onlineReducer'
 // import call from './callReducer'
 // import peer from './peerReducer'
@@ -30,7 +30,7 @@ export default combineReducers({
     suggestions,
     socket,
     notify,
-    // message,
+    message,
     // online,
     // call,
     // peer
